e0521b8d1badb4c2786ac82cbbc264df76389b46
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-01-06 17:53:37 +0530

    Backend API request test successfull from frontend

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
 import "./styles.css";
 import { CounterButton, Link } from "@repo/ui";
+import axios from "axios";
 
 function App(): JSX.Element {
+    React.useEffect(() => {
+        const fetchData = async (): Promise<string> => {
+            return axios.get("http://localhost:5001/message/1");
+        };
+
+        fetchData()
+            .then((resp) => {
+                console.log("resp", resp);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
     return (
         <div className="container">
             <h1 className="title">
