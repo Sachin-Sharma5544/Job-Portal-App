a1ee38568e39891d34c1649021dcf5b7b0cf42e0
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-01-28 23:49:43 +0530

    Input Text field working in debug branch

--- apps/frontend/src/components/navbar/Navbar.tsx
+++ apps/frontend/src/components/navbar/Navbar.tsx
@@ -1,7 +1,16 @@
+import { InputField } from "@repo/ui";
 import React from "react";
 
 const Navbar = (props: any): React.ReactNode => {
-    return <div className="w-full bg-red-500 h-20"></div>;
+    return (
+        <div className="w-full bg-red-500 h-20">
+            <InputField
+                className="w-full text-cyan-900 border-e-slate-50"
+                inputChange={() => true}
+                value="test"
+            />
+        </div>
+    );
 };
 
 export default Navbar;
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,2 +1,3 @@
+export { InputField } from "./TextField";
 export { Link } from "./Link";
 export { CounterButton } from "./CounterButton";
