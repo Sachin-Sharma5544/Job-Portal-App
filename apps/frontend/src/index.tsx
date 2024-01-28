3c9126f4f8468742e9d5a9b309ebb5d42faa06c9
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-01-29 00:13:36 +0530

    Router Implemented

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles.css";
 import { CounterButton, Link } from "@repo/ui";
 import Layout from "./layout/Layout";
@@ -6,9 +7,14 @@ import Layout from "./layout/Layout";
 function App(): JSX.Element {
     return (
         <div className="w-100">
-            <Layout>
-                <h1>children</h1>
-            </Layout>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route element={<h1>children</h1>} path="/test" />
+                    </Routes>
+                </Layout>
+                <CounterButton />
+            </BrowserRouter>
         </div>
     );
 }
