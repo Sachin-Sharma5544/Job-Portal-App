2dbb1fcfda3e34139519a9e92be53862a5c44633
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-03 19:50:55 +0530

    Navbar border issue resolved

--- apps/frontend/src/components/navbar/Navbar.tsx
+++ apps/frontend/src/components/navbar/Navbar.tsx
@@ -1,34 +1,36 @@
 // import { CounterButton, Link, LinkReactRouter } from "@repo/ui";
 import React from "react";
-import { Link, ListsComponents, LinkReactRouter } from "@repo/ui";
+import { LinkReactRouter } from "@repo/ui";
 
 const Navbar = (): React.ReactNode => {
-    const array = ["Test", "Test2", "Test3"];
     return (
         <div className="w-full  bg-slate-600 h-20 ">
             <div className="flex place-items-center border h-full">
-                <div className=" flex place-items-center p-2">
-                    <svg
-                        className="w-10 h-10"
-                        fill="white"
-                        viewBox="0 0 16 16"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            clipRule="evenodd"
-                            d="M11 4V3a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v1H4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1ZM9 2.5H7a.5.5 0 0 0-.5.5v1h3V3a.5.5 0 0 0-.5-.5ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
-                            fillRule="evenodd"
-                        />
-                        <path d="M3 11.83V12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-.17c-.313.11-.65.17-1 .17H4c-.35 0-.687-.06-1-.17Z" />
-                    </svg>
-                    <div className="text-white text-xl pl-2">WorkWise</div>
+                <div className=" flex place-items-center ">
+                    <div className="text-white text-xl pr-2">WorkWise</div>
+                    <div className="pr-10">
+                        <svg
+                            className="w-10 h-10"
+                            fill="white"
+                            viewBox="0 0 16 16"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                clipRule="evenodd"
+                                d="M11 4V3a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v1H4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1ZM9 2.5H7a.5.5 0 0 0-.5.5v1h3V3a.5.5 0 0 0-.5-.5ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+                                fillRule="evenodd"
+                            />
+                            <path d="M3 11.83V12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-.17c-.313.11-.65.17-1 .17H4c-.35 0-.687-.06-1-.17Z" />
+                        </svg>
+                    </div>
                 </div>
                 <div>
-                    {/* <Link href="/home">Home</Link>*/}
-                    <LinkReactRouter to="/">Home</LinkReactRouter>
-                    <LinkReactRouter to="/test">Test</LinkReactRouter>
-                    {/* <Link>Home</Link> */}
-                    {/* <ListsComponents>{array}</ListsComponents> */}
+                    <LinkReactRouter className="" to="/">
+                        Home
+                    </LinkReactRouter>
+                    <LinkReactRouter className="pl-5" to="/test">
+                        Test
+                    </LinkReactRouter>
                 </div>
             </div>
         </div>
--- packages/ui/src/LinkReactRouter/index.tsx
+++ packages/ui/src/LinkReactRouter/index.tsx
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 interface LinkProps {
     to: string;
     children?: React.ReactNode;
+    className: string;
 }
 export function LinkReactRouter(props: LinkProps): JSX.Element {
-    return <Link to={props.to}>{props.children}</Link>;
+    return (
+        <Link className={props.className} to={props.to}>
+            {props.children}
+        </Link>
+    );
 }
