61a7dd5e1420298e5bb2db45445b5f1873ef6489
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-04 22:18:34 +0530

    Navbar component and constant updated

--- packages/constants/src/employee/navbar/index.ts
+++ packages/constants/src/employee/navbar/index.ts
@@ -3,3 +3,5 @@ export const COMPANY = "Company";
 export const SALARY = "Salary";
 export const WORK = "Work";
 export const WISE = "Wise";
+export const LOGIN = "Login";
+export const EMPLOYERS = "Employers/ Post Job";
