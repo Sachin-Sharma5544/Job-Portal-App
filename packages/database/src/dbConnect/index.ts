d7eabc63a3d678cdb743b4e1d2e4043c8e7b6e0f
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-10 23:55:19 +0530

    Db url updated

--- packages/database/src/dbConnect/index.ts
+++ packages/database/src/dbConnect/index.ts
@@ -5,11 +5,11 @@ config();
 
 const dbUserName = process.env.MONGO_USERNAME;
 const dbPassword = process.env.MONGO_PASSWORD;
-let url = process.env
-    .MONGO_DB_URL!.replace("${MONGO_USERNAME}", dbUserName!)
-    .replace("${MONGO_PASSWORD}", dbPassword!);
 
 export const connectDatabase = (dbName: string): Promise<typeof mongoose> => {
-    url = url.replace("${DB_NAME}", dbName);
+    const url = process.env
+        .MONGO_DB_URL!.replace("${MONGO_USERNAME}", dbUserName!)
+        .replace("${MONGO_PASSWORD}", dbPassword!)
+        .replace("${DB_NAME}", dbName);
     return connect(url);
 };
