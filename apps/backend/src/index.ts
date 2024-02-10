bf106a51e0bd4e5c4aa2724527eabf27f61e12d7
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-10 23:23:31 +0530

    database connection code created

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,18 @@
 import { log } from "@repo/logger";
+import { connectDatabase } from "@repo/database";
 import { createServer } from "./server";
 
 const port = process.env.PORT || 5002;
 const server = createServer();
+const dbUrl = process.env.MONGO_DB_URL!;
 
-server.listen(port, () => {
-    log(`api running on ${port}`);
-});
+connectDatabase(dbUrl)
+    .then(() => {
+        log("Database connected successfully");
+        server.listen(port, () => {
+            log(`api running on ${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        log(`Error: ${error.message}`);
+    });
