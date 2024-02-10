c64b2f18dd63dd2f06feca095d881fa93cc21e2f
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-11 00:47:34 +0530

    Database connection created using environment variables

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,15 +2,20 @@ import { log } from "@repo/logger";
 import { connectDatabase } from "@repo/database";
 import { createServer } from "./server";
 
-const port = process.env.PORT || 5002;
+const port = 5002;
 const server = createServer();
 const dbName = process.env.DB_NAME!;
 
-connectDatabase(dbName)
+const url = process.env
+    .MONGO_DB_URL!.replace("${MONGO_USERNAME}", process.env.MONGO_USERNAME!)
+    .replace("${MONGO_PASSWORD}", process.env.MONGO_PASSWORD!)
+    .replace("${DB_NAME}", process.env.DB_NAME!);
+
+connectDatabase(url)
     .then(() => {
         log("Database connected successfully");
         server.listen(port, () => {
-            log(`api running on ${port}`);
+            log(`Server started on ${port}`);
         });
     })
     .catch((error: Error) => {
--- packages/database/src/dbConnect/index.ts
+++ packages/database/src/dbConnect/index.ts
@@ -6,10 +6,6 @@ config();
 const dbUserName = process.env.MONGO_USERNAME;
 const dbPassword = process.env.MONGO_PASSWORD;
 
-export const connectDatabase = (dbName: string): Promise<typeof mongoose> => {
-    const url = process.env
-        .MONGO_DB_URL!.replace("${MONGO_USERNAME}", dbUserName!)
-        .replace("${MONGO_PASSWORD}", dbPassword!)
-        .replace("${DB_NAME}", dbName);
+export const connectDatabase = (url: string): Promise<typeof mongoose> => {
     return connect(url);
 };
