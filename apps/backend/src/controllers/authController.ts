29c6dcacceaefc82fe994c8ed6f78e73de785d19
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-11 12:02:23 +0530

    response returned from controller, MVC architecture running fine

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -1,5 +1,11 @@
 import { type Request, type Response } from "express";
+import { type UserType, user } from "@repo/datatypes";
 
 export const postSignup = (req: Request, res: Response): void => {
-    res.status(201).send({ message: "Hello" });
+    try {
+        const userData: UserType = user.parse(req.body);
+        res.status(201).send({ user: userData });
+    } catch (error: any) {
+        res.status(400).send({ error: error.errors[0].message });
+    }
 };
--- apps/backend/src/routes/authRoute.ts
+++ apps/backend/src/routes/authRoute.ts
@@ -1,16 +1,6 @@
-import { Router, type Request, type Response } from "express";
-import { UserType, user } from "@repo/datatypes";
-import { log } from "@repo/logger";
+import { Router } from "express";
 import { postSignup } from "../controllers/authController";
 
 export const authRouter: Router = Router();
 
-authRouter.post("/", (req: Request, res: Response) => {
-    try {
-        const userData: UserType = user.parse(req.body);
-        res.status(201).send({ user: userData });
-    } catch (error: any) {
-        log(error, ">>>>>>");
-        res.status(400).send({ error: error.errors[0].message });
-    }
-});
+authRouter.post("/", postSignup);
