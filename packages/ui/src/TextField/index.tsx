c3e0f64a8d7c5ab5c4b4abdd3403fa0a1ce6d998
Sachin Kumar Sharma <71969451+Sachin-Sharma5544@users.noreply.github.com>
2024-02-17 17:03:08 +0530

    Feature 4 creating reusable component (#7)
    * Sign In page added
    
    * Styles updated
    
    * Sign In Page styles added
    
    * Styles updated for Login Page
    
    * Signup Page component created
    
    * Login and Signup page component updated
    
    * Confirm password field added on Signup component
    
    * padding added to input fields on Signup page
    
    * Network request on button click to backend is succesful
    
    * Route and controller created for signup
    
    * File name casing rule updated
    
    * Index file for server updated for signup route
    
    * initial setup done for dataTypes package
    
    * Zod dependency installed
    
    * Zod package setup
    
    * Zod implemented
    
    * response returned from controller, MVC architecture running fine
    
    * Signup page data getting saved in Database, its hardcoded but its working fine
    
    * conflicts resolved
    
    * Text field reusable component created
    
    * Signup component creation in progress
    
    * Signup form created using Reusable component
    
    ---------
    
    Co-authored-by: Sachin Sharma <sachin.kumarsharam09@gmail.com>

--- packages/ui/src/TextField/index.tsx
+++ packages/ui/src/TextField/index.tsx
@@ -8,32 +8,24 @@ interface TextFieldProps {
     inputBoxId: string;
     inputType: string;
     inputValue: string;
-    inputChangeHandler: (a: string) => void;
+    changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const index = (props: TextFieldProps): JSX.Element => {
+export function TextField(props: TextFieldProps): JSX.Element {
     return (
         <div>
-            <label
-                className="block text-sm font-medium leading-6 text-gray-900"
-                htmlFor="email"
-            >
+            <label className={props.labelClass} htmlFor={props.labelHtmlFor}>
                 {props.label}
             </label>
             <div className="mt-2">
                 <input
-                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    id="email"
-                    name="email"
-                    onChange={(e) => {
-                        props.inputChangeHandler(e.target.value);
-                    }}
-                    type="email"
+                    className={props.inputBoxClass}
+                    id={props.inputBoxId}
+                    onChange={props.changeHandler}
+                    type={props.inputType}
                     value={props.inputValue}
                 />
             </div>
         </div>
     );
-};
-
-export default index;
+}
