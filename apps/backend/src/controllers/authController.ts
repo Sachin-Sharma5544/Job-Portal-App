0bc9940e672fb13019c2c591123c70e2946c47d4
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-17 12:40:43 +0530

    conflicts resolved

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -7,16 +7,6 @@ export const postSignup = async (
     res: Response
 ): Promise<void> => {
     try {
-<<<<<<< HEAD
-        const userData: UserType = user.parse(req.body);
-
-        const newUser = await UserModel.findOne({ email: userData.email });
-        if (newUser) {
-            throw new Error("User already exists with the email");
-        }
-        const createdUser = await UserModel.create({
-            email: userData.email,
-=======
         const userData: UserType = user.parse(req.body); //user is zod object defined in schema
 
         //check if user with provided email exists or not
@@ -32,9 +22,9 @@ export const postSignup = async (
         //Creating user when user doesnot exists in data base
         const createdUser = await UserModel.create({
             email: userData.email.toLowerCase(),
->>>>>>> 6bc3d11747201d2649f946c88e1c6d3900908122
             password: userData.password,
         });
+
         res.status(201).send({ user: createdUser });
     } catch (error: any) {
         res.status(400).send({ error: error });
