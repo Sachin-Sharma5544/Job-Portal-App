6bc3d11747201d2649f946c88e1c6d3900908122
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-17 12:33:44 +0530

    Comments added in the auth controller

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -7,14 +7,21 @@ export const postSignup = async (
     res: Response
 ): Promise<void> => {
     try {
-        const userData: UserType = user.parse(req.body);
+        const userData: UserType = user.parse(req.body); //user is zod object defined in schema
 
-        const newUser = await UserModel.findOne({ email: userData.email });
+        //check if user with provided email exists or not
+        const newUser = await UserModel.findOne({
+            email: userData.email.toLowerCase(),
+        });
+
+        //Throwing error when user already exists in the database
         if (newUser) {
             throw new Error("User already exists with the email");
         }
+
+        //Creating user when user doesnot exists in data base
         const createdUser = await UserModel.create({
-            email: userData.email,
+            email: userData.email.toLowerCase(),
             password: userData.password,
         });
         res.status(201).send({ user: createdUser });
