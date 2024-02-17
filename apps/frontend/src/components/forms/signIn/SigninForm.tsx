83f7b28b79ccd6e24c5bc991f8f5d7bc4e5b7518
Sachin Kumar Sharma <71969451+Sachin-Sharma5544@users.noreply.github.com>
2024-02-17 22:50:49 +0530

    Feature 3 build signup (#14)
    * Sign In page added
    
    * Styles updated
    
    * Sign In Page styles added
    
    * Styles updated for Login Page
    
    * Signup Page component created
    
    * Login and Signup page component updated
    
    * Confirm password field added on Signup component
    
    * padding added to input fields on Signup page
    
    * Network request on button click to backend is succesful
    
    * Route and controller created for signup
    
    * File name casing rule updated
    
    * Index file for server updated for signup route
    
    * initial setup done for dataTypes package
    
    * Zod dependency installed
    
    * Zod package setup
    
    * Zod implemented
    
    * response returned from controller, MVC architecture running fine
    
    * Signup page data getting saved in Database, its hardcoded but its working fine
    
    * conflicts resolved
    
    * Feature 4 creating reusable component (#9)
    
    * Text field reusable component created
    
    * Feature 4 creating reusable component (#5)
    
    * Sign In page added
    
    * Styles updated
    
    * Sign In Page styles added
    
    * Styles updated for Login Page
    
    * Signup Page component created
    
    * Login and Signup page component updated
    
    * Confirm password field added on Signup component
    
    * padding added to input fields on Signup page
    
    * Network request on button click to backend is succesful
    
    * Route and controller created for signup
    
    * File name casing rule updated
    
    * Index file for server updated for signup route
    
    * initial setup done for dataTypes package
    
    * Zod dependency installed
    
    * Zod package setup
    
    * Zod implemented
    
    * response returned from controller, MVC architecture running fine
    
    * Signup page data getting saved in Database, its hardcoded but its working fine
    
    * conflicts resolved
    
    * Text field reusable component created
    
    ---------
    
    Co-authored-by: Sachin Sharma <sachin.kumarsharam09@gmail.com>
    
    * Signup component creation in progress
    
    * Signup form created using Reusable component
    
    * Feature 4 creating reusable component (#7)
    
    * Sign In page added
    
    * Styles updated
    
    * Sign In Page styles added
    
    * Styles updated for Login Page
    
    * Signup Page component created
    
    * Login and Signup page component updated
    
    * Confirm password field added on Signup component
    
    * padding added to input fields on Signup page
    
    * Network request on button click to backend is succesful
    
    * Route and controller created for signup
    
    * File name casing rule updated
    
    * Index file for server updated for signup route
    
    * initial setup done for dataTypes package
    
    * Zod dependency installed
    
    * Zod package setup
    
    * Zod implemented
    
    * response returned from controller, MVC architecture running fine
    
    * Signup page data getting saved in Database, its hardcoded but its working fine
    
    * conflicts resolved
    
    * Text field reusable component created
    
    * Signup component creation in progress
    
    * Signup form created using Reusable component
    
    ---------
    
    Co-authored-by: Sachin Sharma <sachin.kumarsharam09@gmail.com>
    
    * Issue in typing continuously in Password and Confirm Password field
    
    * Typing issue in password and confirm password field resolved
    
    ---------
    
    Co-authored-by: Sachin Sharma <sachin.kumarsharam09@gmail.com>
    
    * Network request test code added
    
    * Import warning corrected
    
    * Confirm Password validation added when not equal to password
    
    * Error on signup updated
    
    * Login controller function created in the employee backend
    
    * Login and Signup page created using new AuthForm UI Package
    
    ---------
    
    Co-authored-by: Sachin Sharma <sachin.kumarsharam09@gmail.com>

---
+++ apps/frontend/src/components/forms/signIn/SigninForm.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+    JOB_SEEKERS,
+    CREATE_ACCOUNT,
+    REGISTER_BUTTON_TEXT,
+    LOGIN_LINK_TEXT,
+    REGISTER_LINK_TEXT,
+    signupPath,
+    LOGIN_ACCOUNT,
+    LOGIN_BUTTON_TEXT,
+} from "@repo/constants";
+import { LinkReactRouter, Button, TextField, AuthForm } from "@repo/ui";
+
+const SigninForm = () => {
+    const handleLoginClick = async (
+        email: string,
+        password: string,
+        confirmPassword: string
+    ): Promise<void> => {
+        console.log(email, password, confirmPassword);
+    };
+    return (
+        <AuthForm
+            buttonText={LOGIN_BUTTON_TEXT}
+            formHeader={LOGIN_ACCOUNT}
+            isSignup={false}
+            onButtonClick={handleLoginClick}
+            redirectLink={signupPath}
+            redirectLinkText={REGISTER_LINK_TEXT}
+            roleType={JOB_SEEKERS}
+        />
+    );
+};
+
+export default SigninForm;
--- apps/frontend/src/pages/SignUpPage.tsx
+++ apps/frontend/src/pages/SignUpPage.tsx
@@ -5,41 +5,7 @@ import { type UserType } from "@repo/datatypes";
 import SignupForm from "../components/forms/signUp/SignupForm";
 
 const SignUpPage = (): JSX.Element => {
-    return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <div className="flex items-center justify-center">
-                    <div className="text-white mx-2 text-[24px]">
-                        <span className="text-orange-300">{WORK}</span>
-                        <span className="text-sky-400 ">{WISE}</span>
-                    </div>
-
-                    <div className="h-6">
-                        <svg
-                            className="h-full"
-                            fill="black"
-                            viewBox="0 0 16 16"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                clipRule="evenodd"
-                                d="M11 4V3a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v1H4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1ZM9 2.5H7a.5.5 0 0 0-.5.5v1h3V3a.5.5 0 0 0-.5-.5ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
-                                fillRule="evenodd"
-                            />
-                            <path d="M3 11.83V12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-.17c-.313.11-.65.17-1 .17H4c-.35 0-.687-.06-1-.17Z" />
-                        </svg>
-                    </div>
-                </div>
-                <p className="text-center">Job Seekers</p>
-
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Create your Account
-                </h2>
-            </div>
-
-            <SignupForm />
-        </div>
-    );
+    return <SignupForm />;
 };
 
 export default SignUpPage;
