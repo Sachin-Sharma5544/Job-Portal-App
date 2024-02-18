f6b42498c92b3dca448393f3d1d46be12a3b0e3e
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-02-18 09:53:29 +0530

    App running somehow, reason unknown

--- apps/frontend/src/components/forms/signUp/SignupForm.tsx
+++ apps/frontend/src/components/forms/signUp/SignupForm.tsx
@@ -4,21 +4,21 @@ import { JOB_SEEKERS, REGISTER_BUTTON_TEXT } from "@repo/constants";
 import axios from "axios";
 
 const SignupForm = (): JSX.Element => {
-    // const handleRegisterClick = async (
-    //     email: string,
-    //     password: string,
-    //     confirmPassword: string | undefined
-    // ): Promise<void> => {
-    //     console.log(email, password, confirmPassword, " Sign up component");
-    // };
-    // return (
-    //     <AuthForm
-    //         buttonText={REGISTER_BUTTON_TEXT}
-    //         isSignup
-    //         onButtonClick={handleRegisterClick}
-    //         roleType={JOB_SEEKERS}
-    //     />
-    // );
+    const handleRegisterClick = async (
+        email: string,
+        password: string,
+        confirmPassword: string | undefined
+    ): Promise<void> => {
+        console.log(email, password, confirmPassword, " Sign up component");
+    };
+    return (
+        <AuthForm
+            buttonText={REGISTER_BUTTON_TEXT}
+            isSignup
+            onButtonClick={handleRegisterClick}
+            roleType={JOB_SEEKERS}
+        />
+    );
     return <div>Sign Up Form</div>;
 };
 
