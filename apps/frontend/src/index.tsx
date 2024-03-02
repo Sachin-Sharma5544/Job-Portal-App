c65c821bd2f479c2dced2567f7159fb9d7cb1fb9
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-03-02 18:03:09 +0530

    Setup AuthProvider store using react-auth-kit

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -8,31 +8,38 @@ import {
     loginPath,
     signupPath,
 } from "@repo/constants";
+import AuthProvider from "react-auth-kit/AuthProvider";
 import Layout from "./layout/appLayout/AppLayout";
 import SignInPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import { store } from "./auth";
 
 function App(): JSX.Element {
     return (
-        <div>
-            <BrowserRouter>
-                <Layout>
-                    <Routes>
-                        <Route element={<h1>Home Page</h1>} path={homePath} />
-                        <Route
-                            element={<h1>Company Page</h1>}
-                            path={companyPath}
-                        />
-                        <Route
-                            element={<h1>Salary Page</h1>}
-                            path={salaryPath}
-                        />
-                        <Route element={<SignInPage />} path={loginPath} />
-                        <Route element={<SignUpPage />} path={signupPath} />
-                    </Routes>
-                </Layout>
-            </BrowserRouter>
-        </div>
+        <AuthProvider store={store}>
+            <div>
+                <BrowserRouter>
+                    <Layout>
+                        <Routes>
+                            <Route
+                                element={<h1>Home Page</h1>}
+                                path={homePath}
+                            />
+                            <Route
+                                element={<h1>Company Page</h1>}
+                                path={companyPath}
+                            />
+                            <Route
+                                element={<h1>Salary Page</h1>}
+                                path={salaryPath}
+                            />
+                            <Route element={<SignInPage />} path={loginPath} />
+                            <Route element={<SignUpPage />} path={signupPath} />
+                        </Routes>
+                    </Layout>
+                </BrowserRouter>
+            </div>
+        </AuthProvider>
     );
 }
 
