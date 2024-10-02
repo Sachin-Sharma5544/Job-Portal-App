d496728bfa6011e422f73876ffce8c3d6fed2221
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-10-02 18:54:01 +0530

    Redux Saga setup done

--- apps/frontend/src/redux/slices/trendingJobsSlice.ts
+++ apps/frontend/src/redux/slices/trendingJobsSlice.ts
@@ -2,26 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 import { TRENDING_JOBS } from "@repo/constants";
 
 const initialState = {
-    jobs: [
-        { id: 1, jobName: TRENDING_JOBS.MNC },
-        { id: 2, jobName: TRENDING_JOBS.REMOTE },
-        { id: 3, jobName: TRENDING_JOBS.FRESHER },
-        { id: 4, jobName: TRENDING_JOBS.DATA_SCIENCE },
-        { id: 5, jobName: TRENDING_JOBS.HR },
-        { id: 6, jobName: TRENDING_JOBS.ANALYTICS },
-        { id: 7, jobName: TRENDING_JOBS.IT },
-        { id: 8, jobName: TRENDING_JOBS.MARKETING },
-        { id: 9, jobName: TRENDING_JOBS.STARTUP },
-        { id: 10, jobName: TRENDING_JOBS.SUPPLY_CHAIN },
-        { id: 11, jobName: TRENDING_JOBS.HEALTHCARE },
-    ],
+    jobs: [],
 };
 
 export const trendingJobsSlice = createSlice({
     name: "trendingJobs",
     initialState,
-    reducers: {},
+    reducers: {
+        fetchTrendingJobsRequest: (state) => {
+            return state;
+        },
+        fetchTrendingJobsSuccess: (state, action) => {
+            state.jobs = action.payload;
+        },
+        fetchTrendingJobsFailure: (state) => {
+            state.jobs = [];
+        },
+    },
 });
 
-// export const {} = trendingJobsSlice.actions;
+export const {
+    fetchTrendingJobsRequest,
+    fetchTrendingJobsSuccess,
+    fetchTrendingJobsFailure,
+} = trendingJobsSlice.actions;
 export default trendingJobsSlice.reducer;
