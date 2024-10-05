8c275b80139b1d301098850367393dfea713d9de
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-10-06 00:00:01 +0530

    Dropdown field placeholder issue resolved

--- apps/frontend/src/components/home/searchJobs/SearchJobs.tsx
+++ apps/frontend/src/components/home/searchJobs/SearchJobs.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HeaderElement, ParaElement } from "@repo/ui";
+import { Button } from "@repo/ui";
 import { HOME_HEADING_TEXT, HOME_JOB_EXPLORE_TEXT } from "@repo/constants";
 import axios from "axios";
 import AutoCompleteComponent from "../../common/Autocomplete/Autocomplete";
@@ -11,6 +11,7 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 export const SearchJobs = (): JSX.Element => {
     const [location, setLocation] = useState<string>("");
+    const [value, setValue] = useState<number | string>("");
 
     const handleLocation = async (
         e: React.ChangeEvent<HTMLInputElement>
@@ -30,10 +31,10 @@ export const SearchJobs = (): JSX.Element => {
         console.log(data);
     };
     return (
-        <div className="w-full border-2 border-stone-400 flex justify-center items-center rounded-2xl h-18 bg-white">
+        <div className="w-full border-2 border-stone-200 flex justify-center items-center rounded-2xl h-18 bg-white">
             <div className="w-[100%] flex items-center justify-between ">
                 {/*Search Skills*/}
-                <div className="w-1/2 px-[-10px] ">
+                <div className="w-[50%] px-[-10px] ">
                     <AutoCompleteComponent
                         displayLens
                         placeHolder="Enter Skills / Designations / Companies"
@@ -41,9 +42,22 @@ export const SearchJobs = (): JSX.Element => {
                 </div>
 
                 {/*Select  Experience*/}
-                <div className="w-[20%] border-l-2 border-r-2 border-stone-400">
+                <div className="w-[20%] border-l-2 border-r-2 border-stone-200">
                     <FormControl className="w-full">
-                        <Select displayEmpty>
+                        <Select
+                            displayEmpty
+                            onChange={(e) => {
+                                setValue(e.target.value);
+                            }}
+                            renderValue={(selected) => {
+                                if (selected === "") {
+                                    return "Select Experience";
+                                }
+
+                                return selected;
+                            }}
+                            value={value}
+                        >
                             <MenuItem value={10}>Ten</MenuItem>
                             <MenuItem value={20}>Twenty</MenuItem>
                             <MenuItem value={30}>Thirty</MenuItem>
@@ -52,11 +66,17 @@ export const SearchJobs = (): JSX.Element => {
                 </div>
 
                 {/*Search Location*/}
-                <div className="w-[30%] ">
+                <div className="w-[20%] border-r-2 border-stone-200">
                     <AutoCompleteComponent
-                        placeHolder="Search Location"
                         displayLens
+                        placeHolder="Search Location"
                     />
+                </div>
+
+                <div className="w-[10%]">
+                    <Button className=" px-4 text-center py-2 border-2 rounded-3xl w-full text-white bg-slate-800">
+                        Search
+                    </Button>
                 </div>
             </div>
         </div>
--- packages/constants/src/backend/index.ts
+++ packages/constants/src/backend/index.ts
@@ -1,1 +1,1 @@
-export const PORT = 5002;
+export const PORT = 5001;
