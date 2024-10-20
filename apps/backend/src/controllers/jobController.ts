256d9b8233e0c0c1a6930f8bd01b9b18a9b8c20a
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2024-10-21 00:10:49 +0530

    Created aggregate query function to populate jobs based on the the query type

--- apps/backend/src/controllers/jobController.ts
+++ apps/backend/src/controllers/jobController.ts
@@ -21,18 +21,50 @@ export const getJobTextSuggestions = async (
 };
 
 export const getJobs = async (req: Request, res: Response): Promise<void> => {
-    const { jobType } = req.params;
-    try {
-        // const jobs = await Job.find({
-        //     title: { $regex: q, $options: "i" },
-        // }).limit(10);
+    const jobType = req.query.jobType || "";
 
-        // const suggestions = jobs.map((job) => ({
-        //     suggestion: "Test successful",
-        // }));
-        res.status(200).send({ jobs: "Test successful" });
+    try {
+        const jobs = await Job.aggregate([
+            {
+                $lookup: {
+                    from: "companies", // Collection name for companies
+                    localField: "company",
+                    foreignField: "_id",
+                    as: "companyDetails",
+                },
+            },
+            {
+                $unwind: "$companyDetails", // Unwind the joined company details
+            },
+            {
+                $match: {
+                    "companyDetails.tags.businessType": {
+                        $regex: jobType,
+                        $options: "i",
+                    },
+                },
+            },
+            {
+                $project: {
+                    company: 1, // Include fields you need in the final result
+                    title: 1,
+                    description: 1,
+                    location: 1,
+                    salary: 1,
+                    skillsAndTags: 1,
+
+                    //company fields
+                    "companyDetails.name": 1,
+                    "companyDetails.tags": 1,
+                    "companyDetails.rating": 1,
+                    "companyDetails.reviewsCount": 1,
+                },
+            },
+        ]);
+
+        res.status(200).send({ jobs });
     } catch (error) {
-        res.status(400).send({ error: "Test failed" });
+        res.status(400).send({ error });
     }
 };
 
